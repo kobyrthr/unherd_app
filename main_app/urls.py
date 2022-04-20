60e689b54d57774a893fbff1cca7fd7de4e3814c
kobyrthr-dev <kobyrthr@gmail.com>
2022-04-19 22:27:44 -0400

    added search functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 # this like app.use() in express
 urlpatterns = [
-    path('', views.Index.as_view(), name="home")
-
+    path('', views.Index.as_view(), name="home"),
+    path('events/<int:pk>', views.EventDetail.as_view(),name="event_detail")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from pipes import Template
+from pyexpat import model
 from django.shortcuts import render
 from django.views.generic import View
 from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
 from .models import Event
 
 # Create your views here.
@@ -11,7 +13,18 @@ class Index(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["events"] = Event.objects.all()
+        title = self.request.GET.get("title")
+
+        if title != None:
+            # .filter is the sql WHERE statement and name__icontains is doing a search for any name that contains the query param
+            context["events"] = Event.objects.filter(title__icontains=title)
+            context["header"] = f"Searching for {title}"
+        else:
+            context["events"] = Event.objects.all()
+            context["header"] = "Upcoming Events"
         return context
 
-
+class EventDetail(DetailView):
+    model = Event
+    template_name='event_detail.html'
 
