f6a32e65caa81f1d139196fc739bed715cc097e0
kobyrthr-dev <kobyrthr@gmail.com>
2022-04-20 19:15:19 -0400

    added user model to the event model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -18,6 +19,7 @@ class Event(models.Model):
     description = models.CharField(max_length=1000)
     img = models.CharField(max_length=250)
     date = models
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
