a1a9ad968734db17030cec08f8d9b6ed2f7fca92
kobyrthr-dev <kobyrthr@gmail.com>
2022-04-20 19:28:26 -0400

    adding a create view for events

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 # this like app.use() in express
 urlpatterns = [
     path('', views.Index.as_view(), name="home"),
+    path('/new',views.Event_Create.as_view(),name='event_create'),
     path('events/<int:pk>', views.EventDetail.as_view(),name="event_detail")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from pyexpat import model
 from django.shortcuts import render
 from django.views.generic import View
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 from django.views.generic import DetailView
 from .models import Event
 
