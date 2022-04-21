df66c65cb2a3152748d6c8675f34face78805a0a
kobyrthr-dev <kobyrthr@gmail.com>
2022-04-21 17:25:49 -0400

    added event detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('', views.Index.as_view(), name="home"),
     path('new/',views.Event_Create.as_view(),name='event_create'),
-    path('events/<int:pk>', views.EventDetail.as_view(),name="event_detail")
+    path('<int:pk>/', views.EventDetail.as_view(),name="event_detail")
 ]
