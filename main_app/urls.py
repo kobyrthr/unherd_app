077d1f39368b0f80c03e976bc355dfdf73f862e9
kobyrthr-dev <kobyrthr@gmail.com>
2022-04-20 20:18:34 -0400

    added functionality for auth functions

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 # this like app.use() in express
 urlpatterns = [
+	path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup_view, name='signup'),
     path('', views.Index.as_view(), name="home"),
-    path('/new',views.Event_Create.as_view(),name='event_create'),
+    path('new/',views.Event_Create.as_view(),name='event_create'),
     path('events/<int:pk>', views.EventDetail.as_view(),name="event_detail")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,59 @@
 from pipes import Template
 from pyexpat import model
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import View
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 from django.views.generic import DetailView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Event
 
 # Create your views here.
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            print('HEY', user.username)
+            return HttpResponseRedirect('/user'/+str(user))
+        else:
+            return render(request,'signup.html', {'form':form})
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form':form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            u = form.cleaned_date['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                    if user.is_active:
+                        login(request,user)
+                        return HttpResponseRedirect('/user/'+u)
+                    else:
+                        print('The account has been disabled.')
+                        return render(request, 'login.html', {'form':form})
+            else:
+                print('The username and/or password is incorrect.')
+                return render(request, 'login.html', {'form':form})
+        else:
+            return render(request, 'signup.html', {'form':form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html',{'form':form})
+                
+
 class Index(TemplateView):
     template_name= 'index.html'
 
@@ -25,6 +71,11 @@ class Index(TemplateView):
             context["header"] = "Upcoming Events"
         return context
 
+class Event_Create(CreateView):
+    model = Event
+    fields = ['img','title','description','genres']
+    template_name = 'event_create.html'
+    success_url = '/'
 class EventDetail(DetailView):
     model = Event
     template_name='event_detail.html'
