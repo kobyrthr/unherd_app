25512760bf03aa1443d88ac77225c78cf8319a51
kobyrthr-dev <kobyrthr@gmail.com>
2022-04-23 10:32:05 -0400

    registered the rsvp model with django admin. able to CRUD rsvps from the admin portal.

--- main_app/models.py
+++ main_app/models.py
@@ -30,3 +30,6 @@ class RSVP(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
