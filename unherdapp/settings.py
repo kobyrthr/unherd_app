32cf1ba11e1fa34b662f056e056b5ebd3e4df33a
kobyrthr-dev <kobyrthr@gmail.com>
2022-04-26 22:10:06 -0400

    heroku dpeloy attempt

--- unherdapp/settings.py
+++ unherdapp/settings.py
@@ -12,15 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os #important!
-from dotenv import load_dotenv
-load_dotenv()
 
 if not os.environ.get('PRODUCTION'):
     from dotenv import load_dotenv
     load_dotenv()
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
